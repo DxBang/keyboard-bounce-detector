a6db68d9811c53c6c0e02d4d7d0d30c387dee853
Daniel Bang <559371+DxBang@users.noreply.github.com>
2025-01-28 17:45:05 +0100

    1.0.3

--- main.py
+++ main.py
@@ -3,20 +3,55 @@ from keyboard import on_press, wait
 
 BOUNCE_THRESHOLD = 0.075
 
-last_keystroke_time = None
-last_key = None
+timer = None
+last = None
+ignore_key_codes = [
+	42, # Left Shift
+	54, # Right Shift
+	56, # Alt
+	29, # Left Ctrl
+	93, # menu
+	541, # alt gr
+	72, # up arrow
+	80, # down arrow
+	75, # left arrow
+	77, # right arrow
+	82, # insert
+	83, # delete
+	71, # home
+	79, # end
+	73, # page up
+	81, # page down
+	69, # num lock & clear
+	55, # print screen
+	70, # scroll lock
+	58, # caps lock
+#	57, # space
+	14, # backspace
+	-173, # mute
+	-174, # volume down
+	-175, # volume up
+	-176, # play next
+	-177, # play previous
+	-178, # stop
+	-179, # play/pause
+	91, # left windows
+	92, # right windows
+]
 
 def bounce_detection(event):
-	global last_keystroke_time, last_key
-	current_time = time.perf_counter()
-	if last_keystroke_time is not None:
-		time_diff = current_time - last_keystroke_time
-		if event.name == last_key and time_diff < BOUNCE_THRESHOLD:
-			print(f"Bounce detected for key '{event.name}' after {time_diff:.6f} seconds.")
-	last_keystroke_time = current_time
-	last_key = event.name
+	if event.scan_code in ignore_key_codes:
+		return
+	global timer, last
+	now = time.perf_counter()
+	if timer is not None:
+		diff = now - timer
+		if event.name == last.name and diff < BOUNCE_THRESHOLD:
+			print(f"Bounce {diff:.6f}s on ({event.scan_code}) = '{event.name}'")
+	timer = now
+	last = event
 
 on_press(bounce_detection)
 
-print("Listening for keystrokes... Press ESC to exit.")
-wait('esc')
+print("Listening for keystrokes...\nPress F10 to exit.")
+wait("f10")
