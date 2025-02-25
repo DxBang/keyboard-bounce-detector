634a73b06a9bf32a94ec351ff05c39ddca2b2a1a
Daniel Bang <559371+DxBang@users.noreply.github.com>
2025-02-25 12:11:10 +0100

    1.0.5

--- main.py
+++ main.py
@@ -1,57 +1,93 @@
+"""
+Keybounce detection script.
+By: Daniel Bang, Bang Systems
+"""
+
 import time
-from keyboard import on_press, wait
+import keyboard
+import csv
+import json
+from typing import Dict, List
 
-BOUNCE_THRESHOLD = 0.075
-
-timer = None
-last = None
-ignore_keys = [
-	-173, # mute
-	-174, # volume down
-	-175, # volume up
-	-176, # play next
-	-177, # play previous
-	-178, # stop
-	-179, # play/pause
-	14, # backspace
-	29, # Left Ctrl
-	42, # Left Shift
-	54, # Right Shift
-	55, # print screen
-	56, # Alt
-	58, # caps lock
-	69, # num lock & clear
-	70, # scroll lock
-	71, # home
-	72, # up arrow
-	73, # page up
-	75, # left arrow
-	77, # right arrow
-	79, # end
-	80, # down arrow
-	81, # page down
-	82, # insert
-	83, # delete
-	91, # left windows
-	92, # right windows
-	93, # menu
-#	57, # space
-	541, # alt gr
+BOUNCE_THRESHOLD: float = 0.05  # inhumanly fast
+BOUNCE_CSVFILE: str = "results.csv"
+BOUNCE_JSONFILE: str = "results.json"
+IGNORE_KEYS: List[int] = [
+	-173,  # mute
+	-174,  # volume down
+	-175,  # volume up
+	-176,  # play next
+	-177,  # play previous
+	-178,  # stop
+	-179,  # play/pause
 ]
+results: Dict[int, Dict[str, any]] = {}
+bounced: Dict[str, List[float]] = {}
 
-def bounce_detection(event):
-	if event.scan_code in ignore_keys:
+# read the contents of the .version file
+__version__ = "0.0.0"
+with open(".version") as f:
+	__version__ = f.read().strip()
+
+
+def bounce_detection(event: keyboard.KeyboardEvent) -> None:
+	"""Detect key bounce events."""
+	if event.scan_code in IGNORE_KEYS:
 		return
-	global timer, last
 	now = time.perf_counter()
-	if timer is not None:
-		diff = now - timer
-		if event.name == last.name and diff < BOUNCE_THRESHOLD:
-			print(f"Bounce {diff:.6f}s on ({event.scan_code}) = '{event.name}'")
-	timer = now
-	last = event
+	state = event.event_type == 'down'
+	#print(f"({event.scan_code}) = '{event.name}' {event.event_type}")
+	if event.scan_code not in results:
+		results[event.scan_code] = {
+			'name': event.name,
+			'time': now,
+			'state': state,
+			'pressed': 1,
+			'bounced': 0
+		}
+		return
+	key_event = results[event.scan_code]
+	if key_event['state'] == state:
+		# key is held down
+		key_event['time'] = now
+		return
+	# check for bounce
+	diff = now - key_event['time']
+	if state == True and diff < BOUNCE_THRESHOLD:
+		print(f"Bounce {diff:.6f}s on '{event.name}' ({event.event_type})")
+		key_event['bounced'] += 1
+		if event.name not in bounced:
+			bounced[event.name] = []
+		bounced[event.name].append(diff)
+	if state == True:
+		key_event['pressed'] += 1
+	key_event['state'] = state
+	key_event['time'] = now
 
-on_press(bounce_detection)
+def save_results() -> None:
+	"""Save results to CSV and JSON files."""
+	with open(BOUNCE_CSVFILE, 'w', newline='', encoding='utf-8') as f:
+		csvwriter = csv.writer(f, delimiter=',', lineterminator='\n')
+		csvwriter.writerow(['scan_code', 'name', 'pressed', 'bounced'])
+		for scan_code, key_event in results.items():
+			csvwriter.writerow([scan_code, key_event['name'], key_event['pressed'], key_event['bounced']])
+	print(f" Saved CSV: {BOUNCE_CSVFILE}")
+	with open(BOUNCE_JSONFILE, 'w', encoding='utf-8') as f:
+		json.dump({
+			'version': __version__,
+			'bounce': bounced,
+			'result': {k: {kk: vv for kk, vv in v.items() if kk not in ['time', 'state']} for k, v in results.items()},
+		}, f, indent="\t")
+	print(f"Saved JSON: {BOUNCE_JSONFILE}")
 
-print("Listening for keystrokes...\nPress F10 to exit.")
-wait("f10")
+def main() -> None:
+	"""Main function to start key bounce detection."""
+	keyboard.hook(bounce_detection)
+	print(f"Keybounce detection script v{__version__}")
+	print("Start hitting those keys like a maniac.")
+	print("\nPress Ctrl+F10 to save & exit.")
+	keyboard.wait("ctrl+f10")
+	save_results()
+
+if __name__ == "__main__":
+	main()
